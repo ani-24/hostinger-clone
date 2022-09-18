2cc673f217704b180504e1d7f78b7043c5501e2a
Aniket <rajeshwarkumar9204@gmail.com>
2022-09-18 10:37:05 +0530

    add login page

--- login/login.js
+++ login/login.js
@@ -13,3 +13,53 @@ inputs.forEach((el) => {
     }
   });
 });
+
+const form = document.querySelector("form");
+
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+
+const ValidateEmail = () => {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+    return true;
+  }
+  return false;
+};
+
+const eye = document.querySelector(".eye-icon");
+eye.addEventListener("click", () => {
+  if (eye.classList.contains("fa-eye")) {
+    eye.classList.remove("fa-eye");
+    eye.classList.add("fa-eye-slash");
+    password.type = "text";
+  } else {
+    eye.classList.add("fa-eye");
+    eye.classList.remove("fa-eye-slash");
+    password.type = "password";
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (password.value == "") {
+    const label = password.nextElementSibling;
+    password.style.borderColor = "#fc5185";
+    label.classList.add("invalid");
+    password.parentElement.classList.add("animate-err");
+    password.parentElement.addEventListener("animationend", () => {
+      password.parentElement.classList.remove("animate-err");
+    });
+    return false;
+  }
+  if (!ValidateEmail()) {
+    const label = email.nextElementSibling;
+    email.style.borderColor = "#fc5185";
+    label.classList.add("invalid");
+    email.parentElement.classList.add("animate-err");
+    email.parentElement.addEventListener("animationend", () => {
+      email.parentElement.classList.remove("animate-err");
+    });
+    return false;
+  }
+  window.location = "/";
+});
